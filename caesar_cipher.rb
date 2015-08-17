e98a0e393a74d631b857877415e956849952f48b
Tyler Guillen <tyguillen@gmail.com>
2015-08-16 21:39:38 -0700

    commented cipher

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,11 +1,11 @@
 def caesar_cipher(string, shift)
 	output = ""
 	string.each_char do |i|
-		if (65..96) === i.ord
-			output << (((i.ord - 65 + shift) % 26) + 65).chr
-		elsif (96..122) === i.ord
-			output << (((i.ord - 97 + shift) % 26) + 97).chr
-		else
+		if (65..90) === i.ord
+			output << (((i.ord - 65 + shift) % 26) + 65).chr 	# Reduce ASCII value for i, shift it,
+		elsif (96..122) === i.ord														# and account for wraparound (mod 
+			output << (((i.ord - 97 + shift) % 26) + 97).chr 	# 26). Adjust back to the right ASCII
+		else																								# value, and convert to character.
 			output << i
 		end
 	end
